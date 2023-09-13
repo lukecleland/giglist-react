5e9f584a8d6bb0a33b0198d661bc7c765917d940
Luke Cleland <lukecleland@gmail.com>
2023-09-13 21:45:31 +1000

    .

--- src/components/GigAds.tsx
+++ src/components/GigAds.tsx
@@ -1,16 +1,12 @@
 import { useEffect, useState } from "react";
 
 export type GigAd = {
-    Name: string;
     image: { url: string }[];
     link: string;
     Active: boolean;
 };
 
 export const GigAds = ({ adId, gigAds }: { adId: number; gigAds: GigAd[] }) => {
-    const link =
-        gigAds[adId].link +
-        `?utm_source=giglist.com.au&utm_medium=cpc&utm_campaign=${gigAds[adId].Name}]}`;
     return gigAds[adId] ? (
         <div className="advert">
             <a href={gigAds[adId].link} target="_blank" rel="noreferrer">
